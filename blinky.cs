4e0a027e9a5e275c448803d7aff0f6dd7fc9358f
Gerard Sala <12766134+scripting-drafts@users.noreply.github.com>
2019-01-19 17:07:12 +0100

    Add files via upload

---
+++ blinky.cs
@@ -0,0 +1,22 @@
+using System;
+using Raspberry.IO.GeneralPurpose;
+using Raspberry.IO.GeneralPurpose.Behaviors;
+
+namespace blinky
+{
+	class MainClass
+	{
+		public static void Main (string[] args){
+		var led1 = ConnectorPin.P1Pin07.Output();
+
+		var connection = new GpioConnection(led1);
+
+		for (var i = 0; i<100; i++) {
+			connection.Toggle(led1);
+			System.Threading.Thread.Sleep(250);
+		}
+		connection.Close();
+	}
+	}
+}
+
